8a48a416a09f8eefcd17bddbb848fe38725fb27e
shuvo-220 <shuvo.info220@gmail.com>
2025-07-03 20:06:25 +0600

    login ui complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Root />} />
           <Route path='/login' exact element={<Login />} />
-          <Route path='/singup' exact element={<SignUp />} />
+          <Route path='/signup' exact element={<SignUp />} />
           <Route path='/dashboard' exact element={<Home />} />
           <Route path='/income' exact element={<Income />} />
           <Route path='/expense' exact element={<Expense />} />
--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,13 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthLayouts from '../../components/layouts/AuthLayouts'
+import { Link, useNavigate } from 'react-router-dom'
+import Input from '../../components/input/Input'
+import { validateEmail } from '../../utils/helper'
 
 const Login = () => {
+
+  const[email, setEmail] = useState('')
+  const[password, setPassword] = useState('')
+  const[error, setError] = useState(null)
+
+  const navigate = useNavigate();
+
+  const handleLogin=async(e)=>{
+    e.preventDefault()
+    if(!validateEmail(email)){
+      setError('please enter a valid email')
+      return
+    }
+    if(!password){
+      setError('please enter the password')
+      return
+    }
+    setError('')
+    //login api call
+
+  }
+
   return (
-    <div>
+    <AuthLayouts>
       <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
         <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
         <p className='text-xs text-slate-700 mt-[5px] mb-6'>Please enter your details to login</p>
+
+        <form onSubmit={handleLogin}>
+          <Input 
+            value={email}
+            onChange={({target})=>setEmail(target.value)}
+            label='Email Address'
+            placeholder='email@gmail.com'
+            type='text'
+          />
+
+          <Input 
+            value={password}
+            onChange={({target})=>setPassword(target.value)}
+            label='password'
+            placeholder=''
+            type='password'
+          />
+          {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+          <button type='submit' className='btn-primary'>Login</button>
+
+          <p className='text-[13px] text-slate-800 mt-3'>
+            Don't have any account?
+            <Link to='/signup' className='font-medium text-primary underline'>Signup</Link>
+          </p>
+        </form>
+
       </div>
-    </div>
+    </AuthLayouts>
   )
 }
 
